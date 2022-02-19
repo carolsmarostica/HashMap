cdfd3d98a43a885d837d8cbe2e7bd179d99b712e
Carolina Silva Marostica <95856185+carolsmarostica@users.noreply.github.com>
2022-02-19 11:47:39 -0300

    Add files via upload
    Rascunho inicial da função de endereçamento.

---
+++ Trabalho10.c
@@ -0,0 +1,23 @@
+//Trabalho10
+//Caio de Assis Ribeiro e Carolina Silva Marostica
+
+
+#include<stdio.h>
+
+int endereco(int chave, int n){
+    int soma=0, j=0;
+    while(j<12){
+        soma= [soma + (100*chave[j]) + chave[j+1]] % [maximo valor da dobra das chaves minuto 26:50];
+        j=j+2; 
+    }
+    return soma%n;
+}
+
+int main(){
+    int n=13, //numero de chaves
+    op, //menu
+
+    endereço(&chave, )
+
+    return 0;
+}
