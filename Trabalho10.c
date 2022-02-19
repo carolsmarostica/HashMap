7344d7780b8febc5f3f39accdbeb90f25d0c6d8f
Carolina Silva Marostica <95856185+carolsmarostica@users.noreply.github.com>
2022-02-19 12:41:10 -0300

    Update Trabalho10.c

--- Trabalho10.c
+++ Trabalho10.c
@@ -5,24 +5,14 @@
 #include<stdio.h>
 
 int endereco(int chave, int n){
-    int soma=0, j=0;
-    while(j<12){ //12 caracteres fixos 
-        soma= [soma + (100*chave[j]) + chave[j+1]] % 19937;
-        //maximo valor da dobra das chaves minuto 26:50
-        // zz | zz
-        //90+90 | 90+90
-        //9090 | 9090
-        // 18180 -> numero primo 19937
-        j=j+2; 
-    }
-    return soma%n;
+    return chave%n;
 }
 
 int main(){
     int n=13, //numero de chaves
     op, //menu
 
-    endereço(&chave, )
+    endereço(&chave, n);
 
     return 0;
 }
